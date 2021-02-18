beb9251d4fd69cf60d937eb4491e8c61bec16afd
Marco Damaceno <maadamaceno@gmail.com>
2021-02-18 10:57:04 -0300

    Test use case

--- src/TesteCsharp/Program.cs
+++ src/TesteCsharp/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using TesteCsharp.Services.Database;
+using TesteCsharp.Domain.Model;
+using TesteCsharp.Domain.UseCase.Stock;
 
 namespace TesteCsharp
 {
@@ -6,38 +11,47 @@ namespace TesteCsharp
     {
         static void Main(string[] args)
         {
-            Stock stock1 = new Stock("VALE3", 30.50, "Brazil");
-            Stock stock2 = new Stock("CASH3", 16.00, "Brazil");
+            CreateStockInput payload = new CreateStockInput() {
+                Name = "CASH3"
+            };
 
-            Person investor1 = new Person("Marco Damaceno");
-            Transaction transaction1 = new Transaction(investor1, stock1, 3, "compra");
-            transaction1 = investor1.Transact(transaction1);
+            CreateStockOutput stock = CreateStock.handle(payload);
 
-            Company investor2 = new Company("BlackRock");
-            Transaction transaction2 = new Transaction(investor2, stock2, 9, "venda");
-            transaction2 = investor2.Transact(transaction2);
+            Console.WriteLine(JsonSerializer.Serialize(stock));
+            // Stock stock1 = new Stock("VALE3", 30.50, "Brazil");
+            // Stock stock2 = new Stock("CASH3", 16.00, "Brazil");
 
-            Console.WriteLine("========== Relatório de transação nº 1 ==========");
-            Console.WriteLine("Nome: " + transaction1.investor.name);
-            Console.WriteLine("Quantidade: " + transaction1.quantity);
-            Console.WriteLine("Tipo: " + transaction1.type);
-            Console.WriteLine("Ticker: " + transaction1.stock.ticker);
-            Console.WriteLine("Preço: R$ " + transaction1.stock.price);
-            Console.WriteLine("Taxa: R$ " + transaction1.fee);
-            Console.WriteLine("Total: R$ " + transaction1.total);
-            Console.WriteLine("=================================================");
+            // Person investor1 = new Person("Marco Damaceno");
+            // Transaction transaction1 = new Transaction(investor1, stock1, 3, Transaction.TransactionType.BUY);
 
-            Console.WriteLine();
+            // transaction1 = ExchangeService.Order(transaction1);
 
-            Console.WriteLine("========== Relatório de transação nº 2 ==========");
-            Console.WriteLine("Nome: " + transaction2.investor.name);
-            Console.WriteLine("Quantidade: " + transaction2.quantity);
-            Console.WriteLine("Tipo: " + transaction2.type);
-            Console.WriteLine("Ticker: " + transaction2.stock.ticker);
-            Console.WriteLine("Preço: R$ " + transaction2.stock.price);
-            Console.WriteLine("Taxa: R$ " + transaction2.fee);
-            Console.WriteLine("Total: R$ " + transaction2.total);
-            Console.WriteLine("=================================================");
+            // Company investor2 = new Company("BlackRock");
+            // Transaction transaction2 = new Transaction(investor2, stock2, 9, Transaction.TransactionType.SELL);
+
+            // transaction2 = ExchangeService.Order(transaction2);
+
+            // Console.WriteLine("========== Relatório de transação nº 1 ==========");
+            // Console.WriteLine("Nome: " + transaction1.investor.name);
+            // Console.WriteLine("Quantidade: " + transaction1.quantity);
+            // Console.WriteLine("Tipo: " + transaction1.type);
+            // Console.WriteLine("Ticker: " + transaction1.stock.ticker);
+            // Console.WriteLine("Preço: R$ " + transaction1.stock.price);
+            // Console.WriteLine("Taxa: R$ " + transaction1.fee);
+            // Console.WriteLine("Total: R$ " + transaction1.total);
+            // Console.WriteLine("=================================================");
+
+            // Console.WriteLine();
+
+            // Console.WriteLine("========== Relatório de transação nº 2 ==========");
+            // Console.WriteLine("Nome: " + transaction2.investor.name);
+            // Console.WriteLine("Quantidade: " + transaction2.quantity);
+            // Console.WriteLine("Tipo: " + transaction2.type);
+            // Console.WriteLine("Ticker: " + transaction2.stock.ticker);
+            // Console.WriteLine("Preço: R$ " + transaction2.stock.price);
+            // Console.WriteLine("Taxa: R$ " + transaction2.fee);
+            // Console.WriteLine("Total: R$ " + transaction2.total);
+            // Console.WriteLine("=================================================");
         }
     }
 }
